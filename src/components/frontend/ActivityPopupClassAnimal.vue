1aba7b40818eefb8a0f222b3fa7ffc7a1f88ebc0
thanhthien0706 <thanhthien24102002@gmail.com>
2022-05-27 15:15:58 +0700

    thien

---
+++ src/components/frontend/ActivityPopupClassAnimal.vue
@@ -0,0 +1,28 @@
+<style></style>
+
+<template>
+  <Transition
+    enter-active-class="animate__animated animate__zoomIn animate__faster"
+    leave-active-class="animate__animated animate__zoomOut animate__faster"
+  >
+    <div
+      id="ActivityPopupClassAnimal"
+      v-if="statusShowPopup"
+      @click.self="onClosePopup"
+    >
+      <div class="boxPopupInner"></div>
+    </div>
+  </Transition>
+</template>
+
+<script>
+export default {
+  name: "ActivityPopupClassAnimal",
+  props: ["statusShowPopup"],
+  methods: {
+    onClosePopup() {
+      this.$emit("closePopup");
+    },
+  },
+};
+</script>

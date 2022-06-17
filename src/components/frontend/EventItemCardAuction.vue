7835d155226eab5a211583ff251db14914b19faf
Nguyen-Huu-Thuat <nhthuat.20it3@vku.udn.vn>
2022-06-17 19:45:19 +0700

    thuat

---
+++ src/components/frontend/EventItemCardAuction.vue
@@ -0,0 +1,35 @@
+<style lang="scss"></style>
+
+<template>
+  <div class="card_auction">
+    <img class="imagecard" :src="dataItemAuction.img" alt="" />
+    <div class="box__card__inner">
+      <div class="box__header__card">
+        <div class="header__title__card">
+          <p class="title__card">{{ sub_string(dataItemAuction.name, 35) }}</p>
+          <p class="time_card">{{ dataItemAuction.date }}</p>
+        </div>
+      </div>
+      <p class="description__card">
+        {{ sub_string(dataItemAuction.description, 80) }}
+      </p>
+      <div class="btn__card" @click="onEmitShowPopup(dataItemAuction)">GO</div>
+    </div>
+    <!-- <div class="overlay"></div> -->
+  </div>
+</template>
+
+<script>
+import SubString from "@/utils/sub_string";
+
+export default {
+  name: "CardAuction",
+  props: ["dataItemAuction"],
+  methods: {
+    onEmitShowPopup(data) {
+      this.$emit("show-popup", data);
+    },
+    sub_string: SubString,
+  },
+};
+</script>
--- src/views/frontend/EventView.vue
+++ src/views/frontend/EventView.vue
@@ -1,31 +1,39 @@
 <template>
   <DefaultFrontend_1>
     <div id="EventPage" class="animate__animated animate__fadeIn">
-      <!-- <section class="sc__blogs mt-50">
-        <BlogPost />
+      <section class="sc__count mt-50">
+        <event-campaign />
       </section>
-      <section class="sc__blogslide mt-50">
-        <BlogSlide />
+      <section class="sc__count">
+        <event-count />
       </section>
-      <section class="sc__blogs mt-50">
-        <BlogSearch />
-      </section> -->
+      <section class="sc__auction mt-50">
+        <event-auction />
+      </section>
+      <section class="sc__listOrganization mt-50">
+        <event-organization />
+      </section>
     </div>
+    <popup-auction />
   </DefaultFrontend_1>
 </template>
 
 <script>
 import DefaultFrontend_1 from "@/layouts/DefaultFrontend_1.vue";
-// import BlogPost from "@/components/frontend/BlogPost.vue";
-// import BlogSearch from "@/components/frontend/BlogSearch.vue";
-// import BlogSlide from "@/components/frontend/BlogSlide.vue";
+import EventCount from "@/components/frontend/EventCount.vue";
+import EventCampaign from "@/components/frontend/EventCampaign.vue";
+import EventAuction from "@/components/frontend/EventAuction.vue";
+import PopupAuction from "@/components/frontend/EventItemPopupAuction.vue";
+import EventOrganization from "@/components/frontend/EventOrganization.vue";
 export default {
-  name: "BlogView",
+  name: "EventView",
   components: {
     DefaultFrontend_1,
-    // BlogPost,
-    // BlogSearch,
-    // BlogSlide,
+    EventCount,
+    EventCampaign,
+    EventAuction,
+    PopupAuction,
+    EventOrganization,
   },
 };
 </script>

acd33ef203735b684def26abefc74e3e8fe37be9
thanhthien0706 <thanhthien24102002@gmail.com>
2022-09-09 15:44:43 +0700

    thien

--- src/utils/convertImage.js
+++ src/utils/convertImage.js
@@ -1,7 +1,7 @@
 const ConvertImage = (val) => {
   if (val) {
-    const hotname = "http://localhost:3000/";
-    // const hotname = "https://api-love-us.herokuapp.com/";
+    // const hotname = "http://localhost:3000/";
+    const hotname = "https://api-love-us.herokuapp.com/";
     return hotname + val;
   } else {
     return "https://i.pinimg.com/474x/ec/e2/b0/ece2b0f541d47e4078aef33ffd22777e.jpg";
